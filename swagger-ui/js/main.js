d33574894d61f5cf4f1789ffccc845f1feb9f50e
Colm Ó Murchú <colmurp@gmail.com>
2015-04-21 14:37:13 +0100

    Final Temp Style

--- swagger-ui/js/main.js
+++ swagger-ui/js/main.js
@@ -1,1 +1,40 @@
 
+ <script type="text/javascript">
+    $(function () {
+      window.swaggerUi = new SwaggerUi({
+      url: window.location.protocol + "//" + window.location.host,
+      dom_id: "swagger-ui-container",
+      supportedSubmitMethods: ['get', 'post', 'put', 'delete'],
+      onComplete: function(swaggerApi, swaggerUi){
+        log("Loaded SwaggerUI");
+
+        if(typeof initOAuth == "function") {
+          /*
+          initOAuth({
+            clientId: "your-client-id",
+            realm: "your-realms",
+            appName: "your-app-name"
+          });
+          */
+        }
+        $('pre code').each(function(i, e) {
+          hljs.highlightBlock(e)
+        });
+      },
+      onFailure: function(data) {
+        log("Unable to Load SwaggerUI");
+      },
+      docExpansion: "none"
+    });
+
+    $('#input_apiKey').change(function() {
+      var key = $('#input_apiKey')[0].value;
+      log("key: " + key);
+      if(key && key.trim() != "") {
+        log("added key " + key);
+        window.authorizations.add("key", new ApiKeyAuthorization("api_key", key, "query"));
+      }
+    })
+    window.swaggerUi.load();
+  });
+  </script>
